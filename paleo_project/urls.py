e92d282c244c4e92f8c32a65aa33c18603bfa197
BolkaZ <kirillsteam23@gmail.com>
2024-11-14 23:31:04 +0300

    update

--- paleo_project/urls.py
+++ paleo_project/urls.py
@@ -1,14 +1,10 @@
 from django.urls import path
 from app import views
-from app.views import index, getDetailPage, bid_view, add_to_bid
+from app.views import index, getDetailPage, bid_view
 from django.contrib import admin
 
 urlpatterns = [
     path('', index, name='index'),
     path('detail/<int:id>/', getDetailPage, name='getDetailPage'),
     path('bid/', bid_view, name='bid'),
-    path('add-to-bid/<int:period_id>/', views.add_to_bid, name='add_to_bid'),
-    path('admin/', admin.site.urls),
-    path('clear_bid/', views.clear_bid, name='clear_bid'),
-    #path('bid/<int:bid_id>/', bid_view, name='bid_detail'),  # для конкретной заявки
 ]
